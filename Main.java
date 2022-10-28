caca5770490822a4c3349f56c77181416eb39d07
wenaypial <21103866@usc.edu.ph>
2022-10-28 16:33:46 +0800

    save

---
+++ Main.java
@@ -0,0 +1,9 @@
+
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("C mingkoi ayy masaya he's here by my side!!");
+
+		System.out.println("the weather is cold!!");
+	}
+}
--- Newbranch.java
+++
@@ -1,9 +0,0 @@
-
-public class Newbranch
-{
-	public static void main(String[] args) {
-		System.out.println("I like BTS K-pop group!!");
-
-		System.out.println("I love singing also!!");
-	}
-}
